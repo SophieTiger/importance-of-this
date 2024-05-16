11eb1f7c901767d71755ccc95fdffa030b05a4c9
Sophie Tigerholm <sophie.tigerholm@gmail.com>
2024-05-16 14:28:28 +0000

    Add javascript, css and jquery for tests

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
-$("p").on("click" function() {
-    $("p").slideToggle("slow");
+$(document).ready(function() {
+    $("p").click(function() {
+        $(this).slideToggle("slow"); /*this points to the the paragraph you click on, not all */
+    });
+    
+    $("button").mouseenter(function () {
+        $(this).removeClass("makeRed").addClass("makeBlue");
+    });
+    
+    $("button").mouseleave(function () {
+        $(this).removeClass("makeBlue").addClass("makeRed");
+    });
+
+    $(".box").on("click", function(){
+        var classNames = $(this).attr("class").split(" ");
+        $("." + classNames[1]).css("background-color", "red");
+    })
 })
+
